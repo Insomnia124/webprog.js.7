c8a5a8164b188705fa851d89bd99f883f62079fb
Insomnia124 <scernickiy@gmail.com>
2024-10-30 18:37:38 +0200

    asdf

---
+++ JS/task2.js
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+
+=======
+>>>>>>> e1a3b3498f59ced6d475f446b8c3d3761a6a8d00
+function formatMessage(message, maxLength) {
+  if (message.length <= maxLength) {
+      return message;
+  } else {
+      return message.slice(0, maxLength) + "...";
+  }
+}
+
+// Проверка
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+<<<<<<< HEAD
+
+  
+=======
+>>>>>>> e1a3b3498f59ced6d475f446b8c3d3761a6a8d00
